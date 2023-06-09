d5ff6f5310c2ac291163a0a0f4fb4edf4f97f6d1
jimbob <jimbob_dxb0@hotmail.com>
2023-06-09 16:39:16 +0400

    Udpate text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,8 @@ export default function Home() {
       <h1 className="heading">Welcome to my blog</h1>
       <p className="text">
         A delightful destination for learning about DeFi. Join me on an educational journey as we dive into the world of
-        decentralized finance. Together, we'll explore valuable insights and practical knowledge. Buckle up, you're about to
-        read the HOLY GRAIL of shitcoin rules. I hope you enjoy this blog
+        decentralized finance. Together, we&apos;ll explore valuable insights and practical knowledge. Buckle up, you're
+        about to read the HOLY GRAIL of shitcoin rules. I hope you enjoy this blog.
       </p>
       <button className="explore-btn">
         <a href="/blog">Explore</a>
