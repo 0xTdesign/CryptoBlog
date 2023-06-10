8d14ca8d6dc000dbd2fb5163f8c15cc085a83a2f
jimbob <jimbob_dxb0@hotmail.com>
2023-06-10 08:36:50 +0400

    update

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,11 +5,10 @@ export default function page() {
     <div className="container">
       <h1 className="heading">About me</h1>
       <p className="text">
-        I&apos;m thrilled to have you here as we dive into the world of crypto and DeFi. Having lived in Dubai my entire
-        life, I&apos;ve developed a deep passion for coding and helping others in the crypto space. Through this blog, I aim
-        to provide valuable insights, tips, and educational content to empower individuals on their DeFi journey. Let&apos;s
-        unlock the potential of decentralized finance together and explore the fascinating world of crypto. Join me on this
-        exciting adventure.
+        I&apos;m thrilled to have you here as we dive into the world of crypto and DeFi. I&apos;ve developed a deep passion
+        for coding and helping others in the crypto space. Through this blog, I aim to provide valuable insights, tips, and
+        educational content to help individuals on their DeFi journey. Let&apos;s unlock the potential of decentralized
+        finance together and explore the fascinating world of crypto.
       </p>
       <Link href="/blog">
         <button className="explore-btn">Explore</button>
