c5246d39dc83ee9eac1877199a50957c8a74adf8
jimbob <jimbob_dxb0@hotmail.com>
2023-06-12 13:57:09 +0400

    update

--- src/app/components/Footer/Footer.js
+++ src/app/components/Footer/Footer.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./Footer.css";
+import ViewCounter from "../Viewcounter/view";
 
 export default function Footer() {
   return (
     <footer>
+      <ViewCounter />
       <h3>&copy; My Blog </h3>
     </footer>
   );
